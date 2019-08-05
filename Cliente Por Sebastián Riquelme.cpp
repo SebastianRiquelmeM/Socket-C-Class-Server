c501f270983e933a425ed5d217591c749349d5d0
Vodkamina <53723306+Vodkamina@users.noreply.github.com>
2019-08-05 00:06:44 -0400

    Add files via upload

---
+++ Cliente Por Sebastián Riquelme.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+class Client{
+public:
+    WSADATA WSAData;
+    SOCKET server;
+    SOCKADDR_IN addr;
+    char buffer[1024];
+    Client()
+    {
+        cout<<"Conectando al servidor..."<<endl<<endl;
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        server = socket(AF_INET, SOCK_STREAM, 0);
+        addr.sin_addr.s_addr = inet_addr("192.168.0.8");
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(5555);
+        connect(server, (SOCKADDR *)&addr, sizeof(addr));
+        cout << "Conectado al Servidor!" << endl;
+    }
+    void Enviar()
+    {
+        cout<<"Escribe el mensaje a enviar: ";
+        cin>>this->buffer;
+        send(server, buffer, sizeof(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+        cout << "Mensaje enviado!" << endl;
+    }
+    void Recibir()
+    {
+        recv(server, buffer, sizeof(buffer), 0);
+        cout << "El servidor dice: " << buffer << endl;
+        memset(buffer, 0, sizeof(buffer));
+    }
+    void CerrarSocket()
+    {
+       closesocket(server);
+       WSACleanup();
+       cout << "Socket cerrado." << endl << endl;
+    }
+};
+
+int main()
+{
+    Client *Cliente = new Client();
+    while(true)
+    {
+        Cliente->Enviar();
+        Cliente->Recibir();
+    }
+}
---
+++ Servidor  Por Sebastián Riquelme..cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+class Server{
+public:
+    WSADATA WSAData;
+    SOCKET server, client;
+    SOCKADDR_IN serverAddr, clientAddr;
+    char buffer[1024];
+    Server()
+    {
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        server = socket(AF_INET, SOCK_STREAM, 0);
+
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(5555);
+
+        bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+        listen(server, 0);
+
+        cout << "Escuchando para conexiones entrantes." << endl;
+        int clientAddrSize = sizeof(clientAddr);
+        if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+        {
+            cout << "Cliente conectado!" << endl;
+        }
+    }
+
+    string Recibir()
+    {
+      recv(client, buffer, sizeof(buffer), 0);
+      cout << "El cliente dice: " << buffer << endl;
+      memset(buffer, 0, sizeof(buffer));
+    }
+    void Enviar()
+    {
+        cout<<"Escribe el mensaje a enviar: ";
+        cin>>this->buffer;
+        send(client, buffer, sizeof(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+        cout << "Mensaje enviado!" << endl;
+    }
+    void CerrarSocket()
+    {
+        closesocket(client);
+        cout << "Socket cerrado, cliente desconectado." << endl;
+    }
+};
+
+
+int main()
+{
+  Server *Servidor = new Server();
+  while(true)
+  {
+     Servidor->Recibir();
+     Servidor->Enviar();
+  }
+}
